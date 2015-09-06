a005be86b32dacfd744b6ee43fdac8b2d863708b
robin <luobin_it@foxmail.com>
2015-09-06 16:55:14 +0800

    init

--- tddl-sample/src/main/java/com/taobao/tddl/sample/atom/AtomSample.java
+++ tddl-sample/src/main/java/com/taobao/tddl/sample/atom/AtomSample.java
@@ -1,155 +1,1 @@
-/*(C) 2007-2012 Alibaba Group Holding Limited.	 *This program is free software; you can redistribute it and/or modify	*it under the terms of the GNU General Public License version 2 as	* published by the Free Software Foundation.	* Authors:	*   junyu <junyu@taobao.com> , shenxun <shenxun@taobao.com>,	*   linxuan <linxuan@taobao.com> ,qihao <qihao@taobao.com> 	*/	package com.taobao.tddl.sample.atom;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import junit.framework.Assert;
-import org.junit.Test;
-import com.taobao.tddl.sample.base.AtomSampleCase;
-import com.taobao.tddl.sample.util.DateUtil;
-
-/**
- * Comment for AtomBaseSample
- * <p/>
- * Author By: zhuoxue.yll
- * Created Date: 2012-2-29 ÏÂÎç02:26:16 
- */
-@SuppressWarnings("rawtypes")
-public class AtomSample extends AtomSampleCase {
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±íÖĞ²åÈëÊı¾İ
-	 */
-	@Test
-	public void insertByPreStTest() {
-		String sql = "insert into normaltbl_0001 (pk,gmt_create) values (?,?)";
-		Object[] arguments = new Object[] { RANDOM_ID, time };
-		tddlJT.update(sql, arguments);
-
-		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });
-		Assert.assertEquals(time, String.valueOf(re.get("gmt_create")));
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±í¸üĞÂÊı¾İ
-	 */
-	@Test
-	public void updateByPreStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-
-		String sql = "update normaltbl_0001 set gmt_create=? where pk=?";
-		Object[] arguments = new Object[] { nextDay, RANDOM_ID };
-		tddlJT.update(sql, arguments);
-
-		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });
-		Assert.assertEquals(nextDay, String.valueOf(re.get("gmt_create")));
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±íÉ¾³ıÊı¾İ
-	 */
-	@Test
-	public void queryByPreStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-
-		String sql = "select * from normaltbl_0001 where pk=?";
-		Object[] arguments = new Object[] { RANDOM_ID };
-		Map re = tddlJT.queryForMap(sql, arguments);
-		Assert.assertEquals(time, String.valueOf(re.get("gmt_create")));
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±í²éÑ¯Êı¾İ
-	 */
-	@Test
-	public void deleteByPreStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-
-		String sql = "delete from normaltbl_0001 where pk=?";
-		Object[] arguments = new Object[] { RANDOM_ID };
-		tddlJT.update(sql, arguments);
-
-		List re = tddlJT.queryForList("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });
-		Assert.assertEquals(0, re.size());
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±í½øĞĞreplace²Ù×÷
-	 *
-	 */
-	@Test
-	public void replaceByPreStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-		String sql = "replace into normaltbl_0001 (pk,gmt_create) values (?,?)";
-		Object[] arguments = new Object[] { RANDOM_ID, nextDay };
-		tddlJT.update(sql, arguments);
-		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });
-		Assert.assertEquals(nextDay, String.valueOf(re.get("gmt_create")));
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±íÖĞ²åÈëÊı¾İ
-	 */
-	@Test
-	public void insertByStTest() {
-		String sql = "insert into normaltbl_0001 (pk,gmt_create) values (" + RANDOM_ID + ",CURDATE())";
-		tddlJT.update(sql);
-
-		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);
-		Assert.assertEquals(time, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±í¸üĞÂÊı¾İ
-	 */
-	@Test
-	public void updateByStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-
-		String sql = "update normaltbl_0001 set gmt_create= ADDDATE(CURDATE(),INTERVAL 1 DAY) where pk=" + RANDOM_ID;
-		tddlJT.update(sql);
-
-		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);
-		Assert.assertEquals(nextDay, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±íÉ¾³ıÊı¾İ
-	 */
-	@Test
-	public void queryByStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-
-		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);
-		Assert.assertEquals(time, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±í²éÑ¯Êı¾İ
-	 */
-	@Test
-	public void deleteByStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-
-		String sql = "delete from normaltbl_0001 where pk=" + RANDOM_ID;
-		tddlJT.update(sql);
-
-		List re = tddlJT.queryForList("select * from normaltbl_0001 where pk=" + RANDOM_ID);
-		Assert.assertEquals(0, re.size());
-	}
-
-	/**
-	 * ÔÚÖ¸¶¨µÄ±í½øĞĞreplace²Ù×÷
-	 *
-	 */
-	@Test
-	public void replaceByStTest() {
-		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });
-		String sql = "replace into normaltbl_0001 (gmt_create,pk) values (ADDDATE(CURDATE(),INTERVAL 1 DAY),"
-				+ RANDOM_ID + ")";
-		tddlJT.update(sql);
-		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);
-		Assert.assertEquals(nextDay, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));
-	}
-
-}
+/*(C) 2007-2012 Alibaba Group Holding Limited.	 *This program is free software; you can redistribute it and/or modify	*it under the terms of the GNU General Public License version 2 as	* published by the Free Software Foundation.	* Authors:	*   junyu <junyu@taobao.com> , shenxun <shenxun@taobao.com>,	*   linxuan <linxuan@taobao.com> ,qihao <qihao@taobao.com> 	*/	package com.taobao.tddl.sample.atom;import java.util.Date;import java.util.List;import java.util.Map;import junit.framework.Assert;import org.junit.Test;import com.taobao.tddl.sample.base.AtomSampleCase;import com.taobao.tddl.sample.util.DateUtil;/** * Comment for AtomBaseSample * <p/> * Author By: zhuoxue.yll * Created Date: 2012-2-29 ä¸‹åˆ02:26:16  */@SuppressWarnings("rawtypes")public class AtomSample extends AtomSampleCase {	/**	 * åœ¨æŒ‡å®šçš„è¡¨ä¸­æ’å…¥æ•°æ®	 */	@Test	public void insertByPreStTest() {		String sql = "insert into normaltbl_0001 (pk,gmt_create) values (?,?)";		Object[] arguments = new Object[] { RANDOM_ID, time };		tddlJT.update(sql, arguments);		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });		Assert.assertEquals(time, String.valueOf(re.get("gmt_create")));	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨æ›´æ–°æ•°æ®	 */	@Test	public void updateByPreStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		String sql = "update normaltbl_0001 set gmt_create=? where pk=?";		Object[] arguments = new Object[] { nextDay, RANDOM_ID };		tddlJT.update(sql, arguments);		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });		Assert.assertEquals(nextDay, String.valueOf(re.get("gmt_create")));	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨åˆ é™¤æ•°æ®	 */	@Test	public void queryByPreStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		String sql = "select * from normaltbl_0001 where pk=?";		Object[] arguments = new Object[] { RANDOM_ID };		Map re = tddlJT.queryForMap(sql, arguments);		Assert.assertEquals(time, String.valueOf(re.get("gmt_create")));	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨æŸ¥è¯¢æ•°æ®	 */	@Test	public void deleteByPreStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		String sql = "delete from normaltbl_0001 where pk=?";		Object[] arguments = new Object[] { RANDOM_ID };		tddlJT.update(sql, arguments);		List re = tddlJT.queryForList("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });		Assert.assertEquals(0, re.size());	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨è¿›è¡Œreplaceæ“ä½œ	 *	 */	@Test	public void replaceByPreStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		String sql = "replace into normaltbl_0001 (pk,gmt_create) values (?,?)";		Object[] arguments = new Object[] { RANDOM_ID, nextDay };		tddlJT.update(sql, arguments);		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=?", new Object[] { RANDOM_ID });		Assert.assertEquals(nextDay, String.valueOf(re.get("gmt_create")));	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨ä¸­æ’å…¥æ•°æ®	 */	@Test	public void insertByStTest() {		String sql = "insert into normaltbl_0001 (pk,gmt_create) values (" + RANDOM_ID + ",CURDATE())";		tddlJT.update(sql);		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);		Assert.assertEquals(time, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨æ›´æ–°æ•°æ®	 */	@Test	public void updateByStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		String sql = "update normaltbl_0001 set gmt_create= ADDDATE(CURDATE(),INTERVAL 1 DAY) where pk=" + RANDOM_ID;		tddlJT.update(sql);		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);		Assert.assertEquals(nextDay, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨åˆ é™¤æ•°æ®	 */	@Test	public void queryByStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);		Assert.assertEquals(time, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨æŸ¥è¯¢æ•°æ®	 */	@Test	public void deleteByStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		String sql = "delete from normaltbl_0001 where pk=" + RANDOM_ID;		tddlJT.update(sql);		List re = tddlJT.queryForList("select * from normaltbl_0001 where pk=" + RANDOM_ID);		Assert.assertEquals(0, re.size());	}	/**	 * åœ¨æŒ‡å®šçš„è¡¨è¿›è¡Œreplaceæ“ä½œ	 *	 */	@Test	public void replaceByStTest() {		prepareData(tddlJT, "insert into normaltbl_0001 (pk,gmt_create) values (?,?)", new Object[] { RANDOM_ID, time });		String sql = "replace into normaltbl_0001 (gmt_create,pk) values (ADDDATE(CURDATE(),INTERVAL 1 DAY),"				+ RANDOM_ID + ")";		tddlJT.update(sql);		Map re = tddlJT.queryForMap("select * from normaltbl_0001 where pk=" + RANDOM_ID);		Assert.assertEquals(nextDay, DateUtil.formatDate((Date) re.get("gmt_create"), DateUtil.DATE_FULLHYPHEN));	}}
--- tddl-sample/src/main/java/com/taobao/tddl/sample/base/GroupSampleCase.java
+++ tddl-sample/src/main/java/com/taobao/tddl/sample/base/GroupSampleCase.java
@@ -1,58 +1,1 @@
-/*(C) 2007-2012 Alibaba Group Holding Limited.	 *This program is free software; you can redistribute it and/or modify	*it under the terms of the GNU General Public License version 2 as	* published by the Free Software Foundation.	* Authors:	*   junyu <junyu@taobao.com> , shenxun <shenxun@taobao.com>,	*   linxuan <linxuan@taobao.com> ,qihao <qihao@taobao.com> 	*/	package com.taobao.tddl.sample.base;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.springframework.jdbc.core.JdbcTemplate;
-import com.taobao.tddl.common.GroupDataSourceRouteHelper;
-import com.taobao.tddl.jdbc.group.TGroupDataSource;
-
-/**
- * Comment for GroupSampleCase
- * <p/>
- * Author By: zhuoxue.yll
- * Created Date: 2012-2-29 œ¬ŒÁ02:28:32 
- */
-public class GroupSampleCase extends BaseSampleCase {
-
-	protected static JdbcTemplate tddlJT;
-	protected static TGroupDataSource tds;
-
-	@BeforeClass
-	public static void setUp() throws Exception {
-		tddlJT = getJT(GROUP_KEY);
-	}
-
-	@Before
-	public void init() throws Exception {
-		// «Â ˝æ›∑¿÷π∏…»≈
-		String sql = "delete from normaltbl_0001 where pk=?";
-		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(0);
-		clearData(tddlJT, sql, new Object[] { RANDOM_ID });
-		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(1);
-		clearData(tddlJT, sql, new Object[] { RANDOM_ID });
-		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(2);
-		clearData(tddlJT, sql, new Object[] { RANDOM_ID });
-	}
-
-	@After
-	public void destroy() {
-		// «Â ˝æ›∑¿÷π∏…»≈
-		String sql = "delete from normaltbl_0001 where pk=?";
-		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(0);
-		clearData(tddlJT, sql, new Object[] { RANDOM_ID });
-		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(1);
-		clearData(tddlJT, sql, new Object[] { RANDOM_ID });
-		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(2);
-		clearData(tddlJT, sql, new Object[] { RANDOM_ID });
-	}
-
-	protected static JdbcTemplate getJT(String dbGroupKey) {
-		tds = new TGroupDataSource();
-		tds.setAppName(APPNAME);
-		tds.setDbGroupKey(dbGroupKey);
-		tds.init();
-		return new JdbcTemplate(tds);
-	}
-
-}
+/*(C) 2007-2012 Alibaba Group Holding Limited.	 *This program is free software; you can redistribute it and/or modify	*it under the terms of the GNU General Public License version 2 as	* published by the Free Software Foundation.	* Authors:	*   junyu <junyu@taobao.com> , shenxun <shenxun@taobao.com>,	*   linxuan <linxuan@taobao.com> ,qihao <qihao@taobao.com> 	*/	package com.taobao.tddl.sample.base;import org.junit.After;import org.junit.Before;import org.junit.BeforeClass;import org.springframework.jdbc.core.JdbcTemplate;import com.taobao.tddl.common.GroupDataSourceRouteHelper;import com.taobao.tddl.jdbc.group.TGroupDataSource;/** * Comment for GroupSampleCase * <p/> * Author By: zhuoxue.yll * Created Date: 2012-2-29 ‰∏ãÂçà02:28:32  */public class GroupSampleCase extends BaseSampleCase {	protected static JdbcTemplate tddlJT;	protected static TGroupDataSource tds;	@BeforeClass	public static void setUp() throws Exception {		tddlJT = getJT(GROUP_KEY);	}	@Before	public void init() throws Exception {		// Ê∏ÖÊï∞ÊçÆÈò≤Ê≠¢Âπ≤Êâ∞		String sql = "delete from normaltbl_0001 where pk=?";		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(0);		clearData(tddlJT, sql, new Object[] { RANDOM_ID });		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(1);		clearData(tddlJT, sql, new Object[] { RANDOM_ID });		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(2);		clearData(tddlJT, sql, new Object[] { RANDOM_ID });	}	@After	public void destroy() {		// Ê∏ÖÊï∞ÊçÆÈò≤Ê≠¢Âπ≤Êâ∞		String sql = "delete from normaltbl_0001 where pk=?";		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(0);		clearData(tddlJT, sql, new Object[] { RANDOM_ID });		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(1);		clearData(tddlJT, sql, new Object[] { RANDOM_ID });		GroupDataSourceRouteHelper.executeByGroupDataSourceIndex(2);		clearData(tddlJT, sql, new Object[] { RANDOM_ID });	}	protected static JdbcTemplate getJT(String dbGroupKey) {		tds = new TGroupDataSource();		tds.setAppName(APPNAME);		tds.setDbGroupKey(dbGroupKey);		tds.init();		return new JdbcTemplate(tds);	}}
